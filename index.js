0fd6017916ecd2d6d69874dbd69be5be5fa724f5
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2023-08-09 23:15:17 +0530

    modified

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = 8000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
